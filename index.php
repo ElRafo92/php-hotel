04713be5ff5783bffed8719b449431a4e8ea4551
ElRafo92 <rafael.martinez@outlook.it>
2022-12-14 18:28:23 +0100

    stampati i dati in pagina

--- index.php
+++ index.php
@@ -50,9 +50,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <?php foreach($hotels as $hotel) { ?>
+        <li>
+            <h3><?php echo $hotel['name']; ?></h3>
+            <h4><?php echo $hotel['description']; ?></h4>
+            <h4><?php echo $hotel['parking']; ?></h4>
+            <h4><?php echo $hotel['vote']; ?></h4>
+            <h4><?php echo $hotel['distance_to_center']; ?></h4>
+        </li>
+    <?php } ?>
 </body>
 </html>
