d225ec9eeacef17953a9c6f487d0344ea1236cb2
ElRafo92 <rafael.martinez@outlook.it>
2022-12-14 19:17:40 +0100

    riportati dati in tabella

--- index.php
+++ index.php
@@ -51,9 +51,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+    <hr>
     <?php foreach($hotels as $hotel) { ?>
         <li>
             <h3><?php echo $hotel['name']; ?></h3>
@@ -63,5 +65,41 @@
             <h4><?php echo $hotel['distance_to_center']; ?></h4>
         </li>
     <?php } ?>
+    <hr>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to center</th>
+            </tr>
+        </thead>    
+        <tbody>
+            <?php foreach($hotels as $key => $hotel) { ?>
+                <tr>
+                    <th scope="row">
+                        <h3><?php echo $key; ?></h3>
+                    </th>
+                    <td>
+                        <h4><?php echo $hotel['name']; ?></h4>
+                    </td>
+                    <td>
+                        <h4><?php echo $hotel['description']; ?></h4>
+                    </td>
+                    <td>
+                        <h4><?php echo $hotel['parking']; ?></h4>
+                    </td>
+                    <td>
+                        <h4><?php echo $hotel['vote']; ?></h4>
+                    </td>
+                    <td>
+                        <h4><?php echo $hotel['distance_to_center']; ?></h4>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
 </body>
 </html>
